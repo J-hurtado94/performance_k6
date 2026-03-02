18d718bf86d50e44eefad332eef7bc561ba288bf
Juan Carlos Hurtado Martinez <juan.hurtado@globalmvm.com>
2026-03-02 18:06:47 -0500

    ajuste reporte html

--- test.js
+++ test.js
@@ -3,8 +3,14 @@ import {check, sleep } from 'k6';
 
 export const options = {
   // Define the number of iterations for the test
-  vus: 10,
-  iterations: 10,
+  stages: [
+    { duration: '10s', target: 15 },
+    { duration: '20s', target: 15 },
+    { duration: '10s', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<250'],
+  },
 };
 
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
@@ -14,4 +20,14 @@ export default function () {
   check(res, { 'status was 200': (r) => r.status == 200 });
   // Sleep for 1 second to simulate real-world usage
   sleep(1);
+}
+
+
+export function handleSummary(data) {
+  return {
+    'report.html': htmlReport(data),
+    // Si quieres más salidas, descomenta:
+    // 'summary.json': JSON.stringify(data, null, 2),
+    // 'summary.md': textSummary(data, { indent: ' ', enableColors: false }),
+  };
 }
