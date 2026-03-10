5c68cb3617dcc7a6f05b4fed54ed94cf9edbacc7
Juan Carlos Hurtado Martinez <juan.hurtado@globalmvm.com>
2026-03-10 14:13:25 -0500

    se agrega docker compose

--- scripts/test.js
+++ scripts/test.js
@@ -13,9 +13,9 @@ export const options = {
 
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
 export default function () {
-  // Make a GET request to the target URL
+ 
   const res =http.get('https://globalmvm.com/');
   check(res, { 'status was 200': (r) => r.status == 200 });
-  // Sleep for 1 second to simulate real-world usage
+ 
   sleep(1);
 }
